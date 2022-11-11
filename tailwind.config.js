6978571a89d94afd07cdb3ee85de26116dbfa332
shuend <nanawoakira@gmail.com>
2022-11-11 17:26:37 +0700

    portofolio tailwindcss beta (1/2) proly!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#9c8cc6',
+        secondary: '#64748b',
+        dark: '#4a2772',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
 }
